c30e61818a366962c919ea188e6f6279f8e8a0bd
TerryThreatt <terry.threatt@gmail.com>
2020-07-21 12:50:24 -0500

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :tasks
-    has_many :projects, through: :tasks 
+    has_many :projects, through: :tasks
+
+    validates :username, :email, :password, presence: true 
 end
