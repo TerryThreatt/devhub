5f7228545c529b977dea02a2e1bd0d4fea33148d
TerryThreatt <terry.threatt@gmail.com>
2020-07-25 00:49:47 -0500

    fixed association macro bug

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,8 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :project_tasks
-  has_many :tasks, :users through: :project_tasks
-  # has_many :tasks, through: :project_tasks
-  # has_many :users, through: :project_tasks
+  has_many :tasks, through: :project_tasks
+  has_many :users, through: :project_tasks
 
   validates :name, :due_date, presence: true
 end
