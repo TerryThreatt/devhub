417241363e1832068f1c28cc97c571a6ce31ee27
TerryThreatt <terry.threatt@gmail.com>
2020-07-27 20:01:18 -0500

    updated links in welcome controller & views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,4 @@
 class WelcomeController < ApplicationController
   def home
-    if user_signed_in?
-      # @teams = current_user.teams
-      # @projects = current_user.projects
-      @teams = Team.where(id: current_user.id)
-  		@projects = Project.where(team_id: current_user.id)
-    end
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :project_tasks, dependent: :destroy
   has_many :users, through: :project_tasks
-  has_many :tasks. through: :project_tasks
+  has_many :tasks, through: :project_tasks
   # Validations
   validates :name, :due_date, presence: true
 
